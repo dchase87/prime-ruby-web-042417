b1f3ac1ce5e85e8fc92419e883dd1b5b9487c833
David Chase <david.chase4@gmail.com>
2017-04-13 03:17:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(number)
+  new_array = []
+  if number == 1 || number == 0 || number == -1
+    new_array.push(number)
+  end
+  square_root = Math.sqrt(number.abs)
+  range_to_array = (2..square_root).to_a
+  range_to_array.each do |integer|
+    if number % integer == 0
+      new_array.push(integer)
+    end
+  end
+  if new_array.count > 0
+    false
+  else
+    true
+  end
+end
